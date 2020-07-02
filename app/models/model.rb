c57181a7d971ec3e4fae110a17454aef4f1e4443
Romario Gustave <romagustave10@gmail.com>
2020-07-02 21:12:52 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,7 +1,16 @@
 class Dog
   attr_accessor :name, :age 
   attr_accessor :breed 
+  @@dog = [] 
   
+  def initialize(name, age, breed)
+    @name = name 
+    @age = age 
+    @breed = breed 
+  end 
+  
+  def self.all 
+    
   
 
 end
